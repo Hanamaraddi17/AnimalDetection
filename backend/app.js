1d547b9de5f92c1ff410679b89a704806e0ae48f
Reddy <Hanamaraddibb@gmail.com>
2024-11-15 00:00:04 +0530

    app.js updated

--- backend/app.js
+++ backend/app.js
@@ -2,20 +2,35 @@ const express = require('express');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 const animalImageRoutes = require('./routes/animalImageRoutes');
-const path = require('path')
+const path = require('path');
 const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
+
+// CORS Configuration
+const corsOptions = {
+  origin: 'https://animaldetection.vercel.app', // Frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
+
+// Use CORS with the specific configuration
+app.use(cors(corsOptions));
+
+// Middleware to parse incoming JSON requests
 app.use(express.json());
-app.use(cors());
+
+// Routes
 app.use('/auth', authRoutes);
 app.use('/data', animalImageRoutes);
 
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+// Root route
+app.get('/', (req, res) => {
+  res.send("Animal Detection Backend");
+});
 
-app.get('/',(req,res)=>{
-res.send("Animal Detection BACkEnd ")
-})
+// Connect to the database
 connectDB();
+
 module.exports = app;
